6e9ff1ed86c9a79b9bda4f0156362f4bb7a8e1cd
gauravsinhaweb <sinhagaaurav37@gmail.com>
2022-02-22 11:20:20 +0530

    refactor

--- app.js
+++ app.js
@@ -11,11 +11,30 @@ window.onscroll = function () {
 
   prevScrollpos = currentScrollPos;
 };
-
-const toggle = false;
 const hamburger = document.querySelector("#hamburger");
 const aside = document.querySelector("#aside");
 
 hamburger.addEventListener("click", function () {
   aside.style.transform = "translateX(0%)";
+});
+
+const themeBtn = document.querySelector("#theme");
+let On = false;
+themeBtn.addEventListener("click", function () {
+  On = !On;
+  if (On) {
+    document.querySelector(".container").style.backgroundColor = "var(--cta)";
+    document.querySelector("aside").style.backgroundColor = "var(--cta)";
+    document.querySelector("main").style.backgroundColor = "var(--cta)";
+    document.querySelector(".container").style.color = "var(--secondary-li)";
+    document.querySelector("aside").style.color = "var(--secondary-li)";
+    document.querySelector("main").style.color = "var(--secondary-li)";
+  } else if (!On) {
+    document.querySelector(".container").style.backgroundColor = "inherit";
+    document.querySelector("aside").style.backgroundColor = "inherit";
+    document.querySelector("main").style.backgroundColor = "inherit";
+    document.querySelector(".container").style.color = "inherit";
+    document.querySelector("aside").style.color = "inherit";
+    document.querySelector("main").style.color = "inherit";
+  }
 });
